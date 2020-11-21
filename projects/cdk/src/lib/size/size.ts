d3624007783b41154350e4c10942a142db56ee9d
Evgeny Dobryansky <fitz.deve@gmail.com>
2020-11-21 15:59:26 +0200

    added air-theme function for cdk, added sizes to buttons

--- projects/cdk/src/lib/size/size.ts
+++ projects/cdk/src/lib/size/size.ts
@@ -1,31 +1,25 @@
 import { Constructor } from '../types/constructor';
 import { HasElementRef } from '../types/has-element-ref';
-import { CanColor } from '../color/color';
 
 export interface CanSize {
   size: ThemeSizes;
-
-  defaultSize: ThemeSizes;
 }
 
-export type CanSizeCtor = Constructor<CanColor>;
+export type CanSizeCtor = Constructor<CanSize>;
 
 export type ThemeSizes = 'sm' | 'md' | 'lg' | undefined;
 
 
-export function mixinSize<T extends Constructor<HasElementRef>>(base: T, defaultSize?: ThemeSizes): Constructor<HasElementRef> {
+export function mixinSize<T extends Constructor<HasElementRef>>(base: T): CanSizeCtor & T {
   return class extends base {
     // tslint:disable-next-line:variable-name
     private _size: ThemeSizes;
-    defaultSize = defaultSize;
 
     get size(): ThemeSizes {
       return this._size;
     }
 
-    set size(value: ThemeSizes) {
-      const size = value || this.defaultSize;
-
+    set size(size: ThemeSizes) {
       if (size !== this._size) {
         if (this._size) {
           this._elementRef.nativeElement.classList.remove(`size-${this._size}`);
