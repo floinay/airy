5b8f456496fbe0194536550687d031c9cdba660a
Evgeny Dobryansky <fitz.deve@gmail.com>
2021-02-14 21:31:52 +0200

    updated tests

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,12 +1,96 @@
 import { TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
+import {HomeComponent} from './pages/home/home.component';
+import {ButtonsComponent} from './pages/buttons/buttons.component';
+import {TabsComponent} from './pages/tabs/tabs.component';
+import {GridComponent} from './pages/grid/grid.component';
+import {FirstTabComponent} from './pages/tabs/pages/first-tab/first-tab.component';
+import {SecondTabComponent} from './pages/tabs/pages/second-tab/second-tab.component';
+import {ThirdTabComponent} from './pages/tabs/pages/third-tab/third-tab.component';
+import {
+  AirButtonModule,
+  AirCdkModule,
+  AirControlsModule,
+  AirDirectionModule, AirExpansionModule, AirIconModule, AirLayoutModule, AirListModule,
+  AirNavModule,
+  AirTabsModule,
+  AirTooltipModule
+} from '../../projects/kit/src';
+import {RouterModule} from '@angular/router';
+import {BrowserModule} from '@angular/platform-browser';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {AirTypographyModule} from '../../projects/kit/src/typography/air-typography.module';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {HttpClientModule} from '@angular/common/http';
+import {MarkdownModule} from 'ngx-markdown';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [
-        AppComponent
+        AppComponent,
+        HomeComponent,
+        ButtonsComponent,
+        TabsComponent,
+        GridComponent,
+        FirstTabComponent,
+        SecondTabComponent,
+        ThirdTabComponent,
       ],
+      imports: [
+        AirDirectionModule,
+        RouterModule.forRoot([{
+          path: '',
+          component: HomeComponent
+        },
+          {
+            path: 'tabs',
+            component: TabsComponent,
+            children: [
+              {
+                path: 'first',
+                component: FirstTabComponent
+              }, {
+                path: 'second',
+                component: SecondTabComponent
+              }, {
+                path: 'third',
+                component: ThirdTabComponent
+              }
+            ]
+          },
+          {
+            path: 'buttons',
+            component: ButtonsComponent
+          },
+          {
+            path: 'grid',
+            component: GridComponent
+          }]),
+        AirTabsModule,
+        BrowserModule,
+        BrowserAnimationsModule,
+        AirControlsModule,
+        AirTooltipModule,
+        AirNavModule,
+        AirLayoutModule,
+        AirTypographyModule,
+        AirCdkModule.forRoot(),
+        AirButtonModule,
+        AirListModule,
+        AirIconModule.forRoot({
+          pathToStaticIcons: '/assets/icons',
+          defaultFormat: 'svg',
+          pathToDynamicIconsSprite: '/assets/sprites/sprite.svg'
+        }),
+        AirExpansionModule.forRoot({defaultIconName: 'expand', defaultIconSize: 's'}),
+        ReactiveFormsModule,
+        FormsModule,
+        RouterModule,
+        HttpClientModule,
+        MarkdownModule.forRoot()
+      ],
+      providers: [],
     }).compileComponents();
   });
 
@@ -14,18 +98,5 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
     expect(app).toBeTruthy();
-  });
-
-  it(`should have as title 'airy'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('airy');
-  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('airy app is running!');
   });
 });
