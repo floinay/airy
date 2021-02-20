674e48e5631f0f459caaa8c41afe1c4baa3e835d
Evgeny Dobryansky <fitz.deve@gmail.com>
2021-02-20 21:32:24 +0200

    buttons and icons

--- projects/kit/src/icon/components/dynamic-icon/dynamic-icon.component.ts
+++ projects/kit/src/icon/components/dynamic-icon/dynamic-icon.component.ts
@@ -1,14 +1,13 @@
-import { ChangeDetectionStrategy, Component, ElementRef, Inject, Input } from '@angular/core';
-import { ICON_MODULE_OPTIONS } from '../../options';
-import { IconModuleOptions } from '../../options';
-import { CanColorCtor, CanSizeCtor, HasElementRef, mixinColor, mixinSize } from '../../../cdk';
+import {ChangeDetectionStrategy, Component, ElementRef, HostBinding, Inject, Input} from '@angular/core';
+import {ICON_MODULE_OPTIONS, IconModuleOptions} from '../../options';
+import {CanColorCtor, CanSizeCtor, HasElementRef, mixinColor, mixinSize} from '../../../cdk';
 
 const IconBase: CanColorCtor & CanSizeCtor = mixinSize(mixinColor(HasElementRef));
 
 @Component({
   selector: 'air-dynamic-icon',
   template: `
-    <svg [style.stroke]="stroke" [style.fill]="fill">
+    <svg [class.mode-fill]="isFillMode" [class.mode-stroke]="isStrokeMode">
       <use [attr.xlink:href]="src"></use>
     </svg>
   `,
@@ -21,15 +20,25 @@ export class DynamicIconComponent extends IconBase {
   @Input() name = '';
   @Input() sprite = this.options.pathToDynamicIconsSprite;
 
-  @Input() stroke?: 'var(--current-color)' | 'none' | string;
+  @Input() colorMode: 'fill' | 'stroke' | 'all' = 'fill';
 
-  @Input() fill?: 'var(--current-color)' | 'none' | string;
+  get isFillMode(): boolean {
+    console.log(this.colorMode);
+    return this.colorMode === 'fill' || this.colorMode === 'all';
+  }
+
+  @HostBinding('class.mode-stroke')
+  get isStrokeMode(): boolean {
+    return this.colorMode === 'stroke' || this.colorMode === 'all';
+  }
+
 
   get src(): string {
     return `${this.sprite}#${this.name}`;
   }
 
-  constructor(private elementRef: ElementRef, @Inject(ICON_MODULE_OPTIONS) readonly options: IconModuleOptions) {
+  constructor(private elementRef: ElementRef,
+              @Inject(ICON_MODULE_OPTIONS) readonly options: IconModuleOptions) {
     super(elementRef);
   }
 }
