72ea41213f625e7406b0a61bf1f469dd01c8edcb
dimalebid <dima@lebid.pro>
2022-02-15 14:26:34 +0200

    update @airy-ui/cdk

--- projects/cdk/src/lib/pipes/replace-vars/replace-vars.pipe.ts
+++ projects/cdk/src/lib/pipes/replace-vars/replace-vars.pipe.ts
@@ -9,7 +9,7 @@ export class ReplaceVarsPipe implements PipeTransform {
 
   transform(str: string, replaces: Replaces): string {
     return Object.entries(replaces).reduce((accum, [key, value]) => {
-      return accum.replace(`/{{${key}}}/g`, value as string);
+      return accum.replace(new RegExp('{{' + key + '}}', 'g'), value as string);
     }, str);
   }
 
