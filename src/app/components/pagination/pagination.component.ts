998ebffec7587af5bd36fe0d80fbcdc4912722f1
Evgeny Dobryansky <fitz.deve@gmail.com>
2022-02-15 13:32:31 +0200

    fix pagination

--- src/app/components/pagination/pagination.component.ts
+++ src/app/components/pagination/pagination.component.ts
@@ -1,16 +1,15 @@
-import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
-import { map } from 'rxjs/operators';
-import { Observable } from 'rxjs';
-import { Pagination } from '../../../../projects/pagination/src/lib/pagination';
+import {Component, OnInit} from '@angular/core';
+import {ActivatedRoute} from '@angular/router';
+import {map} from 'rxjs/operators';
+import {Observable} from 'rxjs';
+import {Pagination, PaginationCamel} from '../../../../projects/pagination/src/lib/pagination';
 
 @Component({
   selector: 'app-pagination',
   templateUrl: './pagination.component.html',
   styleUrls: ['./pagination.component.scss']
 })
-export class PaginationComponent implements OnInit {
-  pagination$: Observable<Pagination> = this.route.paramMap.pipe(map(m => {
+export class PaginationComponent implements OnInit {pagination$: Observable<PaginationCamel> = this.route.paramMap.pipe(map(m => {
     return {
       total: 200,
       current_page: Number(m.get('page')) || 1,
